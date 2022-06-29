4523976f55a488124dfba22ecb98a901d1071359
CodexRodney <rodney002mwanje@gmail.com>
2022-06-29 13:33:01 +0300

    Deleted Files

--- test.py
+++
@@ -1,9 +0,0 @@
-import bcrypt  
-password = "mypasswordstring"
- 
-# Encode password into a readable utf-8 byte code: 
-password = password.encode('utf-8')
- 
-# Hash the ecoded password and generate a salt: 
-hashedPassword = bcrypt.hashpw(password, bcrypt.gensalt())
-print(hashedPassword)
