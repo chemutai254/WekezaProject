c095ff09192faee3700deff776b05b489d9538f4
CodexRodney <rodney002mwanje@gmail.com>
2022-06-29 13:32:23 +0300

    Moved Files

---
+++ project/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+Defines main
+"""
+
+welcome_message = "______________________________________________________________\n\t\tWEKEZA PROJECT\t\t\n\t\tWE MAKE TRANSACTIONS EASY\t\t\n\t\t$$\t\t$$\n\t\t$$\t\t$$\n\t\t$$\t\t\t$$\n\t\t\t$$\t\t$$\n\t\t\t$$\t$$\t"
+print(welcome_message)
